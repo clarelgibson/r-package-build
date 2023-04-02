19881f08fc741ffe904d0616a654e26c7498be7d
Clare Gibson <clarelgibson@gmail.com>
2023-04-02 14:32:56 +0100

    Adds help text to functions

--- R/tempConversion.R
+++ R/tempConversion.R
@@ -1,19 +1,26 @@
-# Title:        Temperature Conversion
-# Project:      R Package Build
-# Author:       Clare Gibson
-# Created Date: 2023-04-02
-
-# SUMMARY ######################################################################
-# Contains two functions. F_to_C converts temp from Fahrenheit to Celsius. 
-# C_to_F converts temp from Celsius to Fahrenheit.
-
-# F to C
+#' Fahrenheit conversion
+#' 
+#' Convert degrees Fahrenheit temperatures to degrees Celsius
+#' @param f_temp The temperature in degrees Fahrenheit
+#' @return The temperature in degrees Celsius
+#' @examples
+#' temp1 <- F_to_C(50);
+#' temp2 <- F_to_C(c(50,63,23));
+#' @export
 F_to_C <- function(f_temp) {
   c_temp <- (f_temp - 32) * 5/9;
   return(c_temp);
 }
 
-# C to F
+#' Celsius conversion
+#' 
+#' Convert degrees Celsius temperatures to degrees Fahrenheit
+#' @param c_temp The temperature in degrees Celsius
+#' @return The temperature in degrees Fahrenheit
+#' @examples
+#' temp1 <- C_to_F(22);
+#' temp2 <- C_to_F(c(-2,12,23));
+#' @export
 C_to_F <- function(c_temp) {
   f_temp <- (c_temp * 9/5) + 32;
   return(f_temp);
